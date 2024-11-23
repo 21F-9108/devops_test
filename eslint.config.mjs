384e99b8fe55b35c42ee9b4d1e5d4edc4e4c0853
21F-9108 <eishaaqaisar@gmail.com>
2024-11-24 01:03:31 +0500

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 import globals from "globals";
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {languageOptions: { globals: globals.browser }},
-];
+/** @type {import('eslint').Linter.Config} */
+export default {
+  languageOptions: { globals: globals.browser },
+  files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"], // Add files/extensions here
+  ignorePatterns: ["node_modules/"], // Ignore unnecessary folders
+};
 
