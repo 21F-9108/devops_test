d53ab0bd60928d8cdc0726f1ae7be3cba7956b30
21F-9108 <eishaaqaisar@gmail.com>
2024-11-24 01:27:28 +0500

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,10 @@ export default [
     ignores: ["node_modules/"], // Correct way to specify ignored files in flat config
     files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"], // Files to lint
     rules: {
-      "no-unused-vars": "warn", // Example rule (add more as needed)
+      "no-unused-vars": "off", // Example rule (add more as needed)
+     "no-console": "off",
+    "no-empty-function": "warn", 
+        "no-undef": "off"
     },
   },
 ];
